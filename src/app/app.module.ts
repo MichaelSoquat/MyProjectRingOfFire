932316f0c6b5ba3c96d25c255a47e389e30067c4
MichaelSoquat <michael.soquat@gmx.de>
2022-03-04 10:43:44 +0100

    added start screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    StartScreenComponent
   ],
   imports: [
     BrowserModule,
